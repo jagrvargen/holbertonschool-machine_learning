e8bad2da49a9e090ef4ff957dee4acca8ae1a942
jagrvargen <229@holbertonschool.com>
2018-12-11 21:32:41 +0000

    corrected function name

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -3,6 +3,6 @@
 import numpy as np
 
 
-def np_concatenate(mat1, mat2, axis=0):
+def np_cat(mat1, mat2, axis=0):
     """ Concatenates two tensors along a specific axis. """
     return np.concatenate((mat1, mat2), axis=axis)
