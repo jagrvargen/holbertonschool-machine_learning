9225a54a024a37b6d92bad8d6efb06b8af0a6407
jagrvargen <229@holbertonschool.com>
2018-12-11 21:24:42 +0000

    removed numpy import

--- math/0x00-linear_algebra/11-the_western_exchange.py
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import numpy as np
+""" This file contains the np_transpose function """
+
 
 def np_transpose(matrix):
     """ Returns the transpose of an n dimensional tensor. """
-    return np.transpose(matrix)
+    return matrix.transpose()
