799072870e13a28de46effb3f26f8b5f8ab3fe34
jagrvargen <229@holbertonschool.com>
2018-12-11 21:28:37 +0000

    added leading comment to file

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+""" This file contains the np_cat function. """
 import numpy as np
+
 
 def np_concatenate(mat1, mat2, axis=0):
     """ Concatenates two tensors along a specific axis. """
