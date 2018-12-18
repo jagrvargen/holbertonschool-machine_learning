e6b94d54ec5c7023d1d444fab45d0da921133640
jagrvargen <229@holbertonschool.com>
2018-12-18 22:57:56 +0000

    simplified code a bit

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -4,18 +4,15 @@
 
 def np_slice(matrix, axes={}):
     """ Slices a matrix along specific axes. """
-    zero = eval('slice' + str(axes[0])) if 0 in axes else\
-        slice(None, None, None)
-    one = eval('slice' + str(axes[1])) if 1 in axes else\
-        slice(None, None, None)
-    two = eval('slice' + str(axes[2])) if 2 in axes else\
-        slice(None, None, None)
+    s0 = eval('slice' + str((axes[0]))) if 0 in axes else slice(None)
+    s1 = eval('slice' + str(axes[1])) if 1 in axes else slice(None)
+    s2 = eval('slice' + str(axes[2])) if 2 in axes else slice(None)
 
     if len(matrix.shape) == 1:
-        return matrix[zero]
+        return matrix[s0]
     if len(matrix.shape) == 2:
-        return matrix[zero, one]
+        return matrix[s0, s1]
     elif len(matrix.shape) == 3:
-        return matrix[zero, one, two]
+        return matrix[s0, s1, s2]
     else:
         return matrix
