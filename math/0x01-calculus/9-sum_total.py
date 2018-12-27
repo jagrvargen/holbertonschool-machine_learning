f3289c3f0b841f3b01c3d20e80e1107bcdee6e9c
jagrvargen <229@holbertonschool.com>
2018-12-27 19:45:54 +0000

    changed function to use recursion

--- math/0x01-calculus/9-sum_total.py
+++ math/0x01-calculus/9-sum_total.py
@@ -6,6 +6,7 @@ def summation_i_squared(n):
     """ Calculates the summation of i^2 from i = 1 to n """
     if not isinstance(n, int):
         return None
-
-    s = range(1, n+1)
-    return sum(list(map(lambda x: x**2, s)))
+    if n == 1:
+        return 1
+    
+    return n**2 + summation_i_squared(n - 1)
