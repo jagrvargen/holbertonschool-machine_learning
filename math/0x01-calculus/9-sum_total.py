1acd6aeb0382fc183904c47ea472c45fc70bd89d
jagrvargen <229@holbertonschool.com>
2019-01-08 19:19:56 +0000

    reverted disallowed recursion to simple equation

--- math/0x01-calculus/9-sum_total.py
+++ math/0x01-calculus/9-sum_total.py
@@ -4,9 +4,4 @@
 
 def summation_i_squared(n):
     """ Calculates the summation of i^2 from i = 1 to n """
-    if not isinstance(n, int) or n < 1:
-        return None
-    if n == 1:
-        return 1
-
-    return n**2 + summation_i_squared(n - 1)
+    return (n * (n + 1) * (2 * n + 1)) / 6
