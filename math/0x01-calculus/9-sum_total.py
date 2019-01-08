07620c18461a01223517635c15fbcc8c48c83889
jagrvargen <229@holbertonschool.com>
2019-01-08 19:40:20 +0000

    corrected styling error

--- math/0x01-calculus/9-sum_total.py
+++ math/0x01-calculus/9-sum_total.py
@@ -6,5 +6,5 @@ def summation_i_squared(n):
     """ Calculates the summation of i^2 from i = 1 to n """
     if type(n) is not int or n < 1:
         return None
-    
+
     return int((n * (n + 1) * (2 * n + 1)) / 6)
