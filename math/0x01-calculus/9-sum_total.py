c2ceba37acb1cd1d3795d7dcce0a9313a1d1aaa9
jagrvargen <229@holbertonschool.com>
2019-01-08 19:38:45 +0000

    added check for negative integers

--- math/0x01-calculus/9-sum_total.py
+++ math/0x01-calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """ Calculates the summation of i^2 from i = 1 to n """
-    if type(n) is not int:
+    if type(n) is not int or n < 1:
         return None
     
     return int((n * (n + 1) * (2 * n + 1)) / 6)
