6d2b0476b8527a9964e5daa70314a0030cd43a81
jagrvargen <229@holbertonschool.com>
2019-01-08 19:24:06 +0000

    added check for non numerical value of parameter n

--- math/0x01-calculus/9-sum_total.py
+++ math/0x01-calculus/9-sum_total.py
@@ -4,4 +4,7 @@
 
 def summation_i_squared(n):
     """ Calculates the summation of i^2 from i = 1 to n """
+    if not isinstance(int, n) and not isinstance(float, n):
+        return None
+    
     return int((n * (n + 1) * (2 * n + 1)) / 6)
