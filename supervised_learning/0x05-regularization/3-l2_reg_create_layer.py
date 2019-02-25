6b07960f8473cf00ecf82b0d8bd130714f39e9ce
jagrvargen <229@holbertonschool.com>
2019-02-25 23:27:56 +0000

    removed extra newlines

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -18,6 +18,3 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     layer = tf.layers.Dense(n, activation=activation, kernel_initializer=tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"), kernel_regularizer=tf.contrib.layers.l2_regularizer(lambtha),  name="layer")
 
     return layer(prev)
-
-
-
