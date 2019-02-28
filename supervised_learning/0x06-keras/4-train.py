ef7beea421b999cbe7471ee85edeb497501468fc
jagrvargen <229@holbertonschool.com>
2019-02-28 01:01:00 +0000

    adding solution to 4-train

---
+++ supervised_learning/0x06-keras/4-train.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Contains the function def train_model(network, data, labels, batch_size, epochs, verbose=True)
+"""
+import tensorflow as tf
+import tensorflow.keras as keras
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True):
+    """
+    Trains a model using gradient descent.
+    network: keras.Model - The model to train.
+    data: numpy.ndarray (m, nx) - Contains the input data.
+    labels: numpy.ndarray (m, classes) - Contains the correct labels for the data.
+    batch_size: int - The size of the batch used for mini-batch gradient descent.
+    epochs: int - The number of passes through data for gradient descent.
+    verbose: bool - Determines if output should be printed during training.
+
+    Returns: Nothing
+    """
+    network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose)
