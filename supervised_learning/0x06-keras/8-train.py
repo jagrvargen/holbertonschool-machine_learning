791ac5e41ab2af2ec2e06d63e62b3216aef84584
jagrvargen <229@holbertonschool.com>
2019-02-28 19:49:14 +0000

    adding solution to 8-train

---
+++ supervised_learning/0x06-keras/8-train.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Contains the function def train_model(network, data, labels, batch_size, epochs, validation_data=None, early_stopping=False, patience=0, verbose=True)
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, validation_data=None, early_stopping=False, patience=0, learning_rate_decay=False, alpha=0.1, decay_rate=1, save_best=False, filepath=None, verbose=True):
+    """
+    Trains a model using mini-batch gradient descent.
+    network: keras.Model - The model to be trained.
+    data: numpy.ndarray - Contains the input data for the network.
+    labels: numpy.ndarray - Contains the labels for the data.
+    batch_size: int - The size of the batches used in mini-batch gradient descent.
+    epochs: int - The number of passes the model makes through the network.
+    validation_data: tuple (numpy.ndarray, numpy.ndarray) - Contains the
+    validation data and labels used to validate the model.
+    early_stopping: bool - Indicates whether early stopping should be used
+    (if validation_data exists).
+    patience: int - The patience used for early stopping.
+    learning_rate_decay: bool - Indicates whether or not learning rate decay
+    will take place.
+    alpha: float - The initial learning rate.
+    decay_rate: float The rate at which learning rate decay will take place.
+    save_best: bool - Determines whether or not to save the trained model.
+    filepath: str - The path to which to save the trained model.
+    verbose: bool - Used to determine if output should be printed during training.
+
+    Returns: Nothing.
+    """
+    callbacks = []
+    if validation_data:
+        if early_stopping:
+            callbacks.append(K.callbacks.EarlyStopping(patience=patience, verbose=verbose))
+        if learning_rate_decay:
+            def schedule(epoch):
+                return alpha / (1 + decay_rate * epoch)
+            callbacks.append(K.callbacks.LearningRateScheduler(schedule=schedule, verbose=verbose))
+
+    network.fit(data, labels, batch_size=batch_size, epochs=epochs, callbacks=callbacks, validation_data=validation_data, verbose=verbose)
+
+    if save_best:
+        try:
+            old_model = K.models.load_model(filepath)
+            old_loss, _ = old_model.evaluate(data, labels, verbose=verbose)
+            new_loss, _ = network.evaluate(data, labels, verbose=verbose)
+
+            if new_loss < old_loss:
+                network.save(filepath)
+        except:
+            network.save(filepath)
