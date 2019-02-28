9c99e3003616415f115bb24c0577fc1318af4acc
jagrvargen <229@holbertonschool.com>
2019-02-28 00:20:38 +0000

    adding solution to 2-compile

---
+++ supervised_learning/0x06-keras/2-compile.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Contains the function def optimize_model(network, alpha, beta1, beta2)
+"""
+import tensorflow as tf
+import tensorflow.keras as keras
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Compiles a Keras model with Adam optimization.
+    network: keras.Model - The model to optimize.
+    alpha: float - The learning rate.
+    beta1: float - The first parameter of the Adam optimizer.
+    beta2: float - The second parameter of the Adam optimizer.
+
+    Returns: Nothing.
+    """
+    network.compile(optimizer=tf.train.AdamOptimizer(alpha, beta1, beta2))
